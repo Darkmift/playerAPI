b27392fd78c1c3d00fc84f2d03455aceb9bcdd47
Darkmift <Darkmift@users.noreply.github.com>
2018-06-01 19:46:14 +0300

    added updated db

--- playlist.sql
+++ playlist.sql
@@ -2,10 +2,10 @@
 -- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 31, 2018 at 10:56 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Host: localhost
+-- Generation Time: Jun 01, 2018 at 06:45 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -41,12 +41,17 @@ CREATE TABLE `playlists` (
 
 INSERT INTO `playlists` (`id`, `name`, `image`, `songs`) VALUES
 (1, 'pl 1 no songs', 'pl.url', '[{\"name\":\"song1-1\",\"url\":\"song1-1.com\"},{\"name\":\"song1-1\",\"url\":\"song1-1.com\"},{\"name\":\"song1-1\",\"url\":\"song1-1.com\"}]'),
-(2, 'pl 2 no songs', 'pl.url', '[{\"name\":\"song1-1\",\"url\":\"song1-1.com\"}]'),
+(2, 'pl 2 updated again again', 'http://www.image.com', '[{\"name\":\"song1-1\",\"url\":\"song1-1.com\"}]'),
 (3, 'pl 3 no songs', 'pl.url', '[{\"name\":\"song1-1\",\"url\":\"song1-1.com\"}]'),
-(4, 'pl 3 no songs', 'pl.url', '[{\"name\":\"song1-1\",\"url\":\"song1-1.com\"}]'),
-(5, 'pl 3 no songs', 'pl.url', ''),
-(6, 'https://www.w3schools.com', 'str', '[{\"name\":\"sltring\",\"url\":\"https:\\/\\/www.w3schools.com\"},{\"name\":\"string\",\"url\":\"https:\\/\\/www.w3schools.com\"}]'),
-(7, 'listymclistface', 'https://www.w3schools.com', '[{\"name\":\"sltring\",\"url\":\"https:\\/\\/www.w3schools.com\"},{\"name\":\"string\",\"url\":\"https:\\/\\/www.w3schools.com\"}]');
+(4, 'pl 4 no songs', 'pl.url', '[{\"name\":\"song1-1\",\"url\":\"song1-1.com\"}]'),
+(6, 'listymclistface', 'https://www.w3schools.com', '[{\"name\":\"sltring\",\"url\":\"https:\\/\\/www.w3schools.com\"},{\"name\":\"string\",\"url\":\"https:\\/\\/www.w3schools.com\"}]'),
+(7, 'playlist0601', 'http://image.com', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"}]'),
+(14, 'playlist 14 name', 'http://www.plimage.fr', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"},{\"name\":\"some song2\",\"url\":\"http:\\/\\/somesong.com\"}]'),
+(16, 'playlist06012', 'http://www.image.com', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"},{\"name\":\"some song2\",\"url\":\"http:\\/\\/somesong.com\"}]'),
+(17, 'playlist06013', 'http://www.image.com', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"},{\"name\":\"some song2\",\"url\":\"http:\\/\\/somesong.com\"}]'),
+(18, 'playlist06014', 'http://www.image.com', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"},{\"name\":\"some song2\",\"url\":\"http:\\/\\/somesong.com\"}]'),
+(19, 'playlist06015', 'http://www.image.com', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"},{\"name\":\"some song2\",\"url\":\"http:\\/\\/somesong.com\"}]'),
+(20, 'playlist06016', 'http://www.image.com', '[{\"name\":\"some song\",\"url\":\"http:\\/\\/somesong.com\"},{\"name\":\"some song2\",\"url\":\"http:\\/\\/somesong.com\"}]');
 
 --
 -- Indexes for dumped tables
@@ -56,7 +61,8 @@ INSERT INTO `playlists` (`id`, `name`, `image`, `songs`) VALUES
 -- Indexes for table `playlists`
 --
 ALTER TABLE `playlists`
-  ADD UNIQUE KEY `id` (`id`);
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -66,7 +72,7 @@ ALTER TABLE `playlists`
 -- AUTO_INCREMENT for table `playlists`
 --
 ALTER TABLE `playlists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
